4396b9bc3a02ecf5e44f437c088df612c2882e52
Emanuele <emanuelelozzi@gmail.com>
2019-07-16 12:32:40 +0200

    ''

--- threesteplogin/src/main/java/com/example/threesteplogin/NavigatorHost.kt
+++ threesteplogin/src/main/java/com/example/threesteplogin/NavigatorHost.kt
@@ -2,16 +2,23 @@ package com.example.threesteplogin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import androidx.navigation.NavGraph
 import androidx.navigation.fragment.NavHostFragment
 
-class NavigatorHost : AppCompatActivity() {
+class NavigatorHost : AppCompatActivity(){
 
+    private lateinit var nav:NavHostFragment
+    private lateinit var graph:NavGraph
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.navigator_host)
-        var nav = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
-        var graph = nav.navController.navInflater.inflate(R.navigation.nav_graph)
+        nav = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        graph = nav.navController.navInflater.inflate(R.navigation.nav_graph)
+    }
+
+    fun initializeFlux(){
+        graph.startDestination = R.id.step1Fragment
     }
 
     override fun onBackPressed() {}
